57bc849c77e2e09c68348a54687143a22fb9b634
Minhyeok Park <opensource@pmh.codes>
2022-11-19 10:50:20 +0900

    refactor: move person export to service

--- src/persons/persons.controller.ts
+++ src/persons/persons.controller.ts
@@ -3,7 +3,7 @@ import { Response } from 'express'
 import { AuthGuard } from '../auth/auth.guard'
 import { GuardedRequest } from '../auth/dto/Locals.dto'
 import { UsersService } from '../users/users.service'
-import { PersonGender, PersonType } from './entities/person.entity'
+import { PersonType } from './entities/person.entity'
 import { PersonsService } from './persons.service'
 
 @Controller('persons')
@@ -24,74 +24,6 @@ export class PersonsController {
 
     res.set('Content-Type', 'text/csv')
 
-    const persons = await this.personsService.getAllUser()
-
-    const stuKey = '#,이름,학년,반,번호,기숙사명-번호,전화번호,성별'
-    const etcKey = '#,이름,전화번호'
-
-    const headers = '회원관리\n\n재학생,,,,,,,,,졸업생,,,,교사'
-    const keys = `${stuKey},,${etcKey},,${etcKey}`
-
-    const students = persons.filter((v) => v.type === PersonType.CURRENT_STUDENT)
-    const graduates = persons.filter((v) => v.type === PersonType.GRADUATED_STUDENT)
-    const teachers = persons.filter((v) => v.type === PersonType.TEACHER)
-
-    const maxIndex = Math.max(students.length, graduates.length, teachers.length)
-    const phoneFormat = (v: string): string =>
-      v.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, '$1-$2-$3')
-
-    let values = ''
-    for (let i = 0; i < maxIndex; i++) {
-      const student = students[i]
-      const graduate = graduates[i]
-      const teacher = teachers[i]
-      console.log(student, graduate, teacher, i, maxIndex)
-
-      if (student !== undefined) {
-        let studentGender = ''
-        if (student.gender === PersonGender.FEMALE) {
-          studentGender = '여'
-        }
-
-        if (student.gender === PersonGender.MALE) {
-          studentGender = '남'
-        }
-
-        values += [
-          student.id,
-          student.name,
-          student.grade,
-          student.classroom,
-          student.classNumber,
-          student.dormitoryRoomNumber,
-          phoneFormat(student.phone),
-          studentGender
-        ].join(',')
-      } else values += ',,,,,,,'
-
-      values += ',,'
-
-      if (graduate !== undefined) {
-        values += [
-          graduate.id,
-          graduate.name,
-          phoneFormat(graduate.phone)
-        ].join(',')
-      } else values += ',,'
-
-      values += ',,'
-
-      if (teacher !== undefined) {
-        values += [
-          teacher.id,
-          teacher.name,
-          phoneFormat(teacher.phone)
-        ].join(',')
-      } else values += ',,'
-
-      values += '\n'
-    }
-
-    return `${headers}\n${keys}\n${values}`
+    return await this.personsService.exportAllUser()
   }
 }
--- src/persons/persons.service.ts
+++ src/persons/persons.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
-import { Person, PersonType } from './entities/person.entity'
+import { Person, PersonGender, PersonType } from './entities/person.entity'
 
 @Injectable()
 export class PersonsService {
@@ -31,5 +31,76 @@ export class PersonsService {
 
   public async getAllUser (): Promise<Person[]> {
     return await this.persons.find()
+  }
+
+  public async exportAllUser (): Promise<string> {
+    const persons = await this.getAllUser()
+
+    const stuKey = '#,이름,학년,반,번호,기숙사명-번호,전화번호,성별'
+    const etcKey = '#,이름,전화번호'
+
+    const headers = '회원관리\n\n재학생,,,,,,,,,졸업생,,,,교사'
+    const keys = `${stuKey},,${etcKey},,${etcKey}`
+
+    const students = persons.filter((v) => v.type === PersonType.CURRENT_STUDENT)
+    const graduates = persons.filter((v) => v.type === PersonType.GRADUATED_STUDENT)
+    const teachers = persons.filter((v) => v.type === PersonType.TEACHER)
+
+    const maxIndex = Math.max(students.length, graduates.length, teachers.length)
+    const phoneFormat = (v: string): string =>
+      v.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, '$1-$2-$3')
+
+    let values = ''
+    for (let i = 0; i < maxIndex; i++) {
+      const student = students[i]
+      const graduate = graduates[i]
+      const teacher = teachers[i]
+
+      if (student !== undefined) {
+        let studentGender = ''
+        if (student.gender === PersonGender.FEMALE) {
+          studentGender = '여'
+        }
+
+        if (student.gender === PersonGender.MALE) {
+          studentGender = '남'
+        }
+
+        values += [
+          student.id,
+          student.name,
+          student.grade,
+          student.classroom,
+          student.classNumber,
+          student.dormitoryRoomNumber,
+          phoneFormat(student.phone),
+          studentGender
+        ].join(',')
+      } else values += ',,,,,,,'
+
+      values += ',,'
+
+      if (graduate !== undefined) {
+        values += [
+          graduate.id,
+          graduate.name,
+          phoneFormat(graduate.phone)
+        ].join(',')
+      } else values += ',,'
+
+      values += ',,'
+
+      if (teacher !== undefined) {
+        values += [
+          teacher.id,
+          teacher.name,
+          phoneFormat(teacher.phone)
+        ].join(',')
+      } else values += ',,'
+
+      values += '\n'
+    }
+
+    return `${headers}\n${keys}\n${values}`
   }
 }
