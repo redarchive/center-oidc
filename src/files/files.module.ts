2b482f4f878502880b8ce18889e9d2a36cddbb9f
Minhyeok Park <opensource@pmh.codes>
2022-11-23 09:48:54 +0900

    fix: add usermodule dependency to filesmodule

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { FilesService } from './files.service'
 import { FilesController } from './files.controller'
 import { ConfigModule } from '@nestjs/config'
+import { UsersModule } from '../users/users.module'
 
 @Module({
   imports: [
-    ConfigModule
+    ConfigModule,
+    UsersModule
   ],
   controllers: [FilesController],
   providers: [FilesService]
